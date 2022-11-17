07b664e5ea2fbc1818f734a7632b183eafef8e7b
andycandy867 <110787912+andycandy867@users.noreply.github.com>
2022-11-16 20:53:31 -0500

    Delete Blog directory

--- Blog/flaskblog/config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    SECRET_KEY = '2b61ea201abd32d2f2d06c411f9bfc0b'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = ''
-    MAIL_PASSWORD = ''
--- Blog/flaskblog/main/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SearchField
-from wtforms.validators import DataRequired
-
-
-class SearchForm(FlaskForm):
-    searched = SearchField(validators=[DataRequired()])
